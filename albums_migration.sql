8cc999e63bd23902f5994c33b2620ed296aa8bde
Ryan Smith <ryan.l.smith142@gmail.com>
2019-10-09 15:44:32 -0500

    add albums_migration.sql

---
+++ albums_migration.sql
@@ -0,0 +1,12 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(50),
+    name VARCHAR(50),
+    release_date DATE,
+    sales FLOAT,
+    genre VARCHAR(50)
+);
