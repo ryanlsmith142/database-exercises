a088b4359cd1c13af0c5fc635904df6f2ee5c3bd
Ryan Smith <ryan.l.smith142@gmail.com>
2019-10-10 15:32:42 -0500

    add Select statements

---
+++ update_exercises.sql
@@ -0,0 +1,19 @@
+USE codeup_test_db;
+
+-- Write SELECT statements to output each of the following with a caption:
+--
+-- All albums in your table.
+
+    SELECT * FROM albums;
+-- All albums released before 1980
+
+    SELECT * FROM albums WHERE release_date < 1980;
+-- All albums by Michael Jackson
+
+    SELECT * FROM albums WHERE artist = 'Michael Jackson';
+-- After each SELECT add an UPDATE statement to:
+
+-- Make all the albums 10 times more popular (sales * 10)
+-- Move all the albums before 1980 back to the 1800s.
+-- Change 'Michael Jackson' to 'Peter Jackson'
+-- Add SELECT statements after each UPDATE so you can see the results of your handiwork.
