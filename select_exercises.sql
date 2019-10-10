3be9054965e22216dcb0992881f65cacef617ef0
Ryan Smith <ryan.l.smith142@gmail.com>
2019-10-10 15:28:05 -0500

    correct one name

--- select_exercises.sql
+++ select_exercises.sql
@@ -9,7 +9,7 @@ USE codeup_test_db;
 
 SELECT * FROM albums WHERE artist = 'Pink Floyd';
 
-SELECT release_date FROM albums WHERE name = "Sgt. Pepper's Lonely Hearts Club Band";
+SELECT release_date FROM albums WHERE name = "Sgt Pepper's Lonely Hearts Club Band";
 
 SELECT genre FROM albums WHERE name ="Nevermind";
 
