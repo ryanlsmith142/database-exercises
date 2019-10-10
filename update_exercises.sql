02c96f97d051ea7589982435ab2b879e87c3c0ef
Ryan Smith <ryan.l.smith142@gmail.com>
2019-10-10 15:38:43 -0500

    add select and set statements

--- update_exercises.sql
+++ update_exercises.sql
@@ -5,11 +5,20 @@ USE codeup_test_db;
 -- All albums in your table.
 
     SELECT * FROM albums;
+    UPDATE albums;
+    SET sales = (sales*10);
+    SELECT * FROM albums;
 -- All albums released before 1980
 
     SELECT * FROM albums WHERE release_date < 1980;
+    UPDATE albums;
+    SET release_date = release_date - 100 WHERE release_date < 1980;
+    SELECT * FROM albums WHERE release_date < 1980;
 -- All albums by Michael Jackson
 
+    SELECT * FROM albums WHERE artist = 'Michael Jackson';
+    UPDATE albums;
+    SET artist = 'Peter Jackson' WHERE artist = 'Michael Jackson';
     SELECT * FROM albums WHERE artist = 'Michael Jackson';
 -- After each SELECT add an UPDATE statement to:
 
