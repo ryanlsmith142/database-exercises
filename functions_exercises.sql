ceb5229c3ed1dd585355bcb964b2505603d24455
Ryan Smith <ryan.l.smith142@gmail.com>
2019-10-11 15:47:27 -0500

    add function solution

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -66,3 +66,13 @@
 
     SELECT * FROM employees WHERE hire_date LIKE '199%' AND birth_date LIKE '%12-25' ORDER BY birth_date, hire_date desc;
 
+# Update your queries for employees whose names start and end with 'E'. Use concat() to combine their first and last name together as a single column in your results.
+
+    SELECT CONCAT_WS(' ', first_name, last_name) FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
+
+#     For your query of employees born on Christmas and hired in the 90s, use datediff() to find how many days they have been working at the company (Hint: You will also need to use now() or curdate())
+
+SELECT first_name,last_name,CONCAT(
+
+               (((now() - UNIX_TIMESTAMP(hire_date))/60)/60) /24
+           ) AS days_worked FROM employees WHERE hire_date LIKE '199%' AND birth_date LIKE '%12-25';
