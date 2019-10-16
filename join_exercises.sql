5364f81364d3db736f5286aa532d6e90c5c44379
Ryan Smith <ryan.l.smith142@gmail.com>
2019-10-16 14:06:39 -0500

    complete TODOs

--- join_exercises.sql
+++ join_exercises.sql
@@ -35,4 +35,35 @@ WHERE employees.gender = 'F' AND dept_manager.to_date > NOW()
 
 ORDER BY departments.dept_name;
 
+# TODO: Find the current titles of employees currently working in the Customer Service department.
+
+SELECT titles.title, COUNT(titles.title) FROM titles
+
+JOIN employees ON employees.emp_no = titles.emp_no
+
+JOIN dept_emp ON dept_emp.emp_no = employees.emp_no
+
+JOIN departments on departments.dept_no = dept_emp.dept_no
+
+WHERE departments.dept_name = 'Customer Service' AND titles.to_date > NOW()
+
+GROUP BY titles.title;
+
+# TODO: Find the current salary of all current managers.
+
+SELECT departments.dept_name, CONCAT(employees.first_name, ' ', employees.last_name) AS 'Full Name', salaries.salary
+
+FROM salaries
+
+JOIN employees ON employees.emp_no = salaries.emp_no
+
+JOIN dept_manager ON employees.emp_no = dept_manager.emp_no
+
+JOIN departments ON dept_manager.dept_no = departments.dept_no
+
+WHERE dept_manager.to_date > NOW() AND salaries.to_date > NOW()
+
+ORDER BY departments.dept_name;
+
+
 
